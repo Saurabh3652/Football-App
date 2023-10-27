c731ed4b4f4fa54be9b1ee0b540c28102361c054
Saurabh3652 <saurabh36521@gmail.com>
2023-10-27 18:24:04 +0530

    team name changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <footer className="footer items-center p-3 drop-shadow-lg bg-neutral text-neutral-content mt-4">
       <div className="grid-flow-col gap-2 justify-self-end">
-        <p>Created by Pulkit Gupta</p>
+        <p>Created by Saurabh Patel</p>
         <a
-          href="https://github.com/pulkitbest/football-app"
+          href="https://github.com/Saurabh3652"
           target="_blank"
           rel="noreferrer"
         >
